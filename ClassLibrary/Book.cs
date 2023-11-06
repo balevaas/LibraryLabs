881fb1ff307ca94fdbebbc0fd74f86fc12ef0217
balevaas <72543214+balevaas@users.noreply.github.com>
2023-11-06 19:30:20 +0700

    Добавьте файлы проекта.

---
+++ ClassLibrary/Book.cs
@@ -0,0 +1,26 @@
+﻿namespace ClassLibrary
+{
+    public class Book
+    {
+        public string Article { get; set; }
+        public string Author { get; set; }
+        public string Name { get; set; }
+        public int Year { get; set; }
+        public int Count { get; set; }
+
+        public Book(string article, string author, string name, int year, int count)
+        {
+            Article = article;
+            Author = author;
+            Name = name;
+            Year = year;
+            Count = count;
+        }
+        public string ToString()
+        {
+            string result = $"Артикул: {Article}, Автор: {Author}, Название: {Name}\nГод издания: {Year}, Количество {Count}";
+            return result;
+        }
+
+    }
+}
---
+++ ClassLibrary/Reader.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Xml.Linq;
+
+namespace ClassLibrary
+{
+    public class Reader
+    {
+        public string FIO {  get; set; }
+        public int NumberGroop { get; set; }
+        public int CountBook { get; set; }
+        public Reader(string fio, int numberGroop, int countBook)
+        {
+            FIO = fio;
+            NumberGroop = numberGroop;
+            CountBook = countBook;
+        }
+        public string ToString()
+        {
+            string result = $"ФИО: {FIO}, Номер группы: {NumberGroop}, Количество книг: {CountBook}";
+            return result;
+        }
+    }
+}
